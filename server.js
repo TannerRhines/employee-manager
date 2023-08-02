c9dcc9c39978cf27efb4b04f5d3e9e8e696f7d79
Tanner Rhines <tannerrhines@protonmail.com>
2023-08-02 00:23:47 -0800

    created add department and add new employee consts

--- server.js
+++ server.js
@@ -1,10 +1,89 @@
 const inquirer = require('enquirer');
+const cTable = require("console.table");
 
 const sql = require('./db/query_lib');
 
 
+// adds a new department
+const newDept = async () => {  
+
+    const deptartment = await inquirer.prompt([
+       {
+         type: "input",
+         name: "name",
+         message: "What is the name of the Department",
+         validate: (name) =>{
+           if (name) {
+             return true;
+           } else {
+             console.log(" Please Enter a Department Name!")
+             return false;
+           }
+         },
+      },
+    ]);
+  
+    await sql.addDept(deptartment);
+  
+    chooseRequest();
+  }
 
 
+  // adds an employee
+const newEmp = async () => {
+
+    const roleArr = await cHelper.roleChoices();
+  
+    const mgmtArr = await cHelper.mgmtChoices();
+  
+    const emp = await inquirer.prompt([
+        {
+          type: "input",
+          name: "first",
+          message: "What is the Employees First Name?",
+          validate: (first) =>{
+            if (first && isNaN(first)) {
+              return true;
+            } else {
+              console.log(" Please Enter a Name!")
+              return false;
+            }
+          },
+       },
+       {
+        type: "input",
+        name: "last",
+        message: "What is the Employees Last Name?",
+        validate: (last) =>{
+          if (last && isNaN(last)) {
+            return true;
+          } else {
+            console.log(" Please Enter a Name!")
+            return false;
+          }
+        },
+      },
+      {
+        type: "list",
+        name: 'role_id',
+        message: "What is the Employees Role?",
+        choices: roleArr,
+        loop: false,
+      },
+      {
+        type: "list",
+        name: 'manager_id',
+        message: "Who is the Employees Manager?",
+        choices: mgmtArr,
+        loop: false,
+      }
+     ]);
+  
+    await sql.addEmp(emp);
+  
+    chooseRequest();  
+   
+  }
 
 
 
